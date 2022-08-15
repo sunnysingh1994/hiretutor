f279590e7ad42a0c3064962c61dddd8aa65a67b7
sunny singh <sunnysingh1994.ss@gmail.com>
2022-08-15 15:57:34 +0530

    CRUD operation completed

---
+++ script.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+   
+/*------------------------------------------
+--------------------------------------------
+parse application/json
+--------------------------------------------
+--------------------------------------------*/
+app.use(bodyParser.json());
+   
+/*------------------------------------------
+--------------------------------------------
+Database Connection
+--------------------------------------------
+--------------------------------------------*/
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', /* MySQL User */
+  password: 'sunny', /* MySQL Password */
+  database: 'expertdata' /* MySQL Database */
+});
+   
+/*------------------------------------------
+--------------------------------------------
+Shows Mysql Connect
+--------------------------------------------
+--------------------------------------------*/
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected with App...');
+});
+   
+/**
+ * Get All Items
+ *
+ * @return response()
+ */
+app.get('/api/experts',(req, res) => {
+  let sqlQuery = "SELECT * FROM expertinfo";
+  
+  let query = conn.query(sqlQuery, (err, results) => {
+    if(err) throw err;
+    res.send(apiResponse(results));
+  });
+});
+   
+/**
+ * Get Single Item
+ *
+ * @return response()
+ */
+app.get('/api/experts/:id',(req, res) => {
+  let sqlQuery = "SELECT * FROM expertinfo WHERE id=" + req.params.id;
+    
+  let query = conn.query(sqlQuery, (err, results) => {
+    if(err) throw err;
+    res.send(apiResponse(results));
+  });
+});
+   
+/**
+ * Create New Item
+ *
+ * @return response()
+ */
+app.post('/api/experts',(req, res) => {
+  let data = { firstName: req.body.firstName, lastName: req.body.lastName, description: req.body.description, languages: req.body.languages, cost: req.body.cost, image: req.body.image };
+  console.log("getting data", data);
+  let sqlQuery = "INSERT INTO expertinfo SET ?"; 
+  //firstName = ?,lastName = ?,description = ?,languages = ?,cost = ?,image = ?
+  let query = conn.query(sqlQuery, data,(err, results) => {
+    if(err) throw err;
+    res.send(apiResponse(results));
+  });
+});
+   
+/**
+ * Update Item
+ *
+ * @return response()
+ */
+app.put('/api/experts/:id',(req, res) => {
+  let sqlQuery = "UPDATE expertinfo SET firstName='"+req.body.firstName+"', lastName='"+req.body.lastName+"',description='"+req.body.description+"', cost='"+req.body.cost+"',image='"+req.body.image+"' WHERE id="+req.params.id;
+  
+  let query = conn.query(sqlQuery, (err, results) => {
+    if(err) throw err;
+    res.send(apiResponse(results));
+  });
+});
+   
+/**
+ * Delete Item
+ *
+ * @return response()
+ */
+app.delete('/api/experts/:id',(req, res) => {
+  let sqlQuery = "DELETE FROM expertinfo WHERE id="+req.params.id+"";
+    
+  let query = conn.query(sqlQuery, (err, results) => {
+    if(err) throw err;
+      res.send(apiResponse(results));
+  });
+});
+  
+/**
+ * API Response
+ *
+ * @return response()
+ */
+function apiResponse(results){
+    return JSON.stringify({"status": 200, "error": null, "response": results});
+}
+   
+/*------------------------------------------
+--------------------------------------------
+Server listening
+--------------------------------------------
+--------------------------------------------*/
+//Establish the server connection
+//PORT ENVIRONMENT VARIABLE
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
